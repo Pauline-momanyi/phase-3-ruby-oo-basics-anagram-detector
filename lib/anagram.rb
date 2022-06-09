7747df0891d75d868baa205120cc8bcda602fdfe
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-09 18:58:28 +0300

    commit tree 1185f52785f3abaab318d59f599d1b7b2ea571a5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize word
+        @word = word
+    end
+
+    def match(text)
+        # text.select {|letter| letter.split("").sort == @word.split("").sort}
+        new_text = text.split(" ")
+        # new_text
+        new_text.select {|letter| letter.split("").sort == @word.split("").sort}
+    end  
+end
+
+sent = Anagram.new('popo')
+# puts sent.match(%w[popo is opop me])
+p sent.match('name is popo')
+
+
